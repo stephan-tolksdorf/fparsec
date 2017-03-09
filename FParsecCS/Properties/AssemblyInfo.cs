5abe457481b8e0b181e3e3f9e3d12dc7814ffa12
Marcus Griep <mgriep@cimpress.com>
2017-03-09 07:44:30 -0500

    Reimplement strong naming

--- FParsecCS/Properties/AssemblyInfo.cs
+++ FParsecCS/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion  (FParsec.CommonAssemblyInfo.FileVersion)]
 [assembly: AssemblyConfiguration(FParsec.CommonAssemblyInfo.CSConfiguration)]
 
-[assembly: InternalsVisibleTo (FParsec.CommonAssemblyInfo.FParsecAssemblyName)]
-[assembly: InternalsVisibleTo (FParsec.CommonAssemblyInfo.TestAssemblyName)]
+[assembly: InternalsVisibleTo (FParsec.CommonAssemblyInfo.FParsecAssemblyName + ", " + FParsec.CommonAssemblyInfo.StrongNamePublicKey)]
+[assembly: InternalsVisibleTo (FParsec.CommonAssemblyInfo.TestAssemblyName + ", " + FParsec.CommonAssemblyInfo.StrongNamePublicKey)]
 
 namespace FParsec {
 
@@ -48,6 +48,13 @@ internal static partial class CommonAssemblyInfo {
     public const string FSConfiguration = Configuration;
     public const string FParsecAssemblyName = "FParsec";
     public const string TestAssemblyName = "Test";
+
+    public const string StrongNamePublicKey =
+        "PublicKey=002400000480000094000000060200000024000052534131000400000100010077c6be48a40f5b" +
+        "194ec9f992e5b512bbbba33e211354d9ee50c3214decddad8356470a9a19a9ee84637cbd6ff690" +
+        "9527d3973741dbe0a69b1461eeae774af9a78de45618ffd6fe7c7d52e0441b92f3bc7e8fb5757f" +
+        "b8b1611a0b6b8c9f9ef64edcf51d44218ae040f3015373fd261d30f8e1f5a1f914fd9ebcde7d7e" +
+        "f42dbaa5";
 #endif
 };
 
