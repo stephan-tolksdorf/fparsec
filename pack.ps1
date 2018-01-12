ec8819e99d367d4b4e7072c9b169f11a750374e9
Cameron Taggart <cameron.taggart@gmail.com>
2018-01-11 19:54:13 -0500

    build on appveyor

--- pack.ps1
+++ pack.ps1
@@ -13,6 +13,10 @@ Param(
 
 $ErrorActionPreference = 'Stop'
 
+$version = '1.0.4'
+$packageVersion = $version + '-RC'
+if ($env:appveyor){ $packageVersion = packageVersion + 'pdb' + [int]::Parse($env:appveyor_build_number).ToString('000') }
+
 $configs = $('Release-LowTrust', 'Release')
 
 $testTargetFrameworks = @{'Release'          = $('net45', 'net40-client')
@@ -38,7 +42,7 @@ foreach ($config in $configs) {
     $props = "/p:Configuration=$config /p:VersionSuffix=$versionSuffix /p:FParsecNuGet=true"
     invoke "msbuild /t:Restore $props"
     invoke "msbuild /t:Clean $props"
-    invoke "msbuild FParsec /t:Build /p:SourceLinkCreate=true $props"
+    invoke "msbuild FParsec /t:Build /p:SourceLinkCreate=true $props /p:Version=$version"
     invoke "msbuild Test /t:Build $props"
     foreach ($tf in $testTargetFrameworks[$config]) {
         if ($tf.StartsWith('netcoreapp')) {
@@ -48,7 +52,7 @@ foreach ($config in $configs) {
         }
     }
     invoke "msbuild /t:Restore $props /p:MergedFParsecPackage=true"
-    invoke "msbuild /t:Pack /p:NoBuild=true $props /p:MergedFParsecPackage=true"
+    invoke "msbuild /t:Pack /p:NoBuild=true $props /p:MergedFParsecPackage=true /p:PackageVersion=$packageVersionu"
     if (($config -eq 'Release-LowTrust') -and $testPCL) {
         $pclProps = "/p:TargetFramework=net45 /p:OutputPath=bin\$config\net45-pcl\ /p:TestPCLFParsec=true $props"
         invoke "msbuild Test/Test.fsproj /t:Clean $pclProps"
